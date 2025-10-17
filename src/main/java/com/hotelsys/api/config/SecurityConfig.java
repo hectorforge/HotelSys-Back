c9ef73b0ee45a90d6eeb7573310ebfc988377de2
Jesús López <un.diletante@hotmail.com>
2025-10-17 11:11:47 -0500

    1. Creación de Dockerfile, archivo necesario para dockerizar el api. 2. Modifique el POM para bajar la versión de java a 21, necesario para dockerizar (aún no existe imágenes de docker con java 25). 3. Modificaciones en SecurityConfig.java y WebConfig.java para modificar la configuración de CORS para pemitir el acceso local y desde el el dominio ees-peru.com. 4. Creación de archivos schema.sql y data.sql y modificación de application.properties para que cree automaticamente la base de datos (si no existe) y tablas , y también inserte los datos de prueba en la base de datos.

--- src/main/java/com/hotelsys/api/config/SecurityConfig.java
+++ src/main/java/com/hotelsys/api/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -34,5 +39,22 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Orígenes permitidos
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://web.ees-peru.com"));
+        // Permitimos patrones para subdominios si se necesita
+        configuration.setAllowedOriginPatterns(List.of("https://*.ees-peru.com"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // aplicar a todas las rutas (ajusta si quieres restringir)
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
