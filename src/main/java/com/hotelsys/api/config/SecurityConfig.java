9bce0fdd5a956d39535d275c3564333c2593b9af
Jesús López <un.diletante@hotmail.com>
2025-10-24 22:28:57 -0500

    Actualizaciones para mostrar las imagenes de las habitaciones del listado de inicio desde el servidor.

--- src/main/java/com/hotelsys/api/config/SecurityConfig.java
+++ src/main/java/com/hotelsys/api/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         http.cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/**").permitAll()
+                        req.requestMatchers("/api/**", "/img/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
