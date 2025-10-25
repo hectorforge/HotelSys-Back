3418f5a4a74652bea02b3d641c71772b7c683615
Jesús López <un.diletante@hotmail.com>
2025-10-24 22:19:53 -0500

    Actualizaciones para mostrar las imagenes del listado desde el servidor

--- src/main/java/com/hotelsys/api/config/SecurityConfig.java
+++ src/main/java/com/hotelsys/api/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         http.cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/**").permitAll()
+                        req.requestMatchers("/api/**", "/img/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
