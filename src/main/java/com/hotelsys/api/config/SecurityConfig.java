3db5f2261bc5980e1d189f84c2e80513b94bd1a3
Gabriel <gabrielfloresgaragundo.1999@gmail.com>
2025-10-24 20:57:31 -0500

    feat: actulizado

--- src/main/java/com/hotelsys/api/config/SecurityConfig.java
+++ src/main/java/com/hotelsys/api/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers("/api/**").permitAll()
-                                .anyRequest().permitAll()
+                                .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
