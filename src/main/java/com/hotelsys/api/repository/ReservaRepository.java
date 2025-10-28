028f1dd7348d12403983620a31762e13201d926f
pablocfr <i202407970@cibertec.edu.pe>
2025-10-27 20:03:52 -0500

    Ultimo cambios API

--- src/main/java/com/hotelsys/api/repository/ReservaRepository.java
+++ src/main/java/com/hotelsys/api/repository/ReservaRepository.java
@@ -3,6 +3,9 @@ package com.hotelsys.api.repository;
 import com.hotelsys.api.model.catalogos.EstadoReserva;
 import com.hotelsys.api.model.entidades.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,5 +14,5 @@ public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
     List<Reserva> findByCliente_Email(String email);
 
-
+    List<Reserva> findByEstadoReservaAndFechaReservaBefore(EstadoReserva estadoReserva, LocalDateTime fechaReserva);
 }
